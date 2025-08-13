13f0beaa62d8bb3fb560382bf844a3fe06fc0009
revy <revy@cnt.id>
2025-08-13 10:46:31 +0700

    Bismillah: Add loading state

---
+++ app/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="fixed inset-0 bg-white/80 flex items-center justify-center z-50">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-4 border-[#F26532]"></div>
+    </div>
+  );
+}
